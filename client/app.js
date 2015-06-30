a50640603f14598daafc9a00c41b9d811e6d9eca
Stewart McCoy <mccoy.stewart@gmail.com>
2015-06-30 13:31:35 -0700

    first pass at stars prototype

---
+++ client/app.js
@@ -0,0 +1,19 @@
+(function (angular) {
+  'use strict';
+  angular.module('stars', [
+    'ui.router'
+ ,'stars.main'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/stars/main');
+
+    $stateProvider
+      .state('stars', {
+        url: '/stars',
+        abstract: true,
+        template: '<ui-view></ui-view>'
+      });
+  });
+}(angular));
+
+
+
---
+++ client/common/directives.js
@@ -0,0 +1,10 @@
+(function (angular) {
+  "use strict";
+
+  angular.module('stars')
+  .directive('search', function () {
+    return {
+
+    };
+  });
+}(angular));
